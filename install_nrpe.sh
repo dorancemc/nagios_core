fd42d3b11d55d692f15ae456ae27231d556261e2
Dorance Martinez Cortes <dorancemc@gmail.com>
2016-11-09 21:45:40 -0500

    update script
    se agregó el path de instalación como variable
    valida si el usuario nagios existe y lo crea
    valida si existe un archivo nrpe.cfg previo y hace backup

--- install_nrpe.sh
+++ install_nrpe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# @dorancemc - 10-sep-2016
+# @dorancemc - 09-nov-2016
 #
 # Script para installar nrpe
 # Validado en : Debian 6+, Ubuntu 16+, Centos 6+
@@ -30,6 +30,57 @@ linux_variant() {
 
 command_exists () {
     type "$1" &> /dev/null ;
+}
+
+debian() {
+  if [ $version -ge 8 ]; then
+#!/bin/bash
+#
+# @dorancemc - 10-sep-2016
+#
+# Script para installar nrpe
+# Validado en : Debian 6+, Ubuntu 16+, Centos 6+
+#
+#
+
+NRPE_version="3.0.1"
+TEMP_PATH="/tmp/nagios_`date +%Y%m%d%H%M%S`"
+INSTALL_PATH="/opt/nagios"
+NAGIOS_USER="nagios"
+
+linux_variant() {
+  if [ -f "/etc/debian_version" ]; then
+    if ! command_exists lsb_release ; then
+      apt-get install -y lsb-release
+    fi
+    distro=$(lsb_release -s -i | tr '[:upper:]' '[:lower:]')
+    flavour=$(lsb_release -s -c )
+    version=$(lsb_release -s -r | cut -d. -f1 )
+  elif [ -f "/etc/redhat-release" ]; then
+    distro="rh"
+    flavour=$(cat /etc/redhat-release | cut -d" " -f1 | tr '[:upper:]' '[:lower:]' )
+    version=$(cat /etc/redhat-release | grep -oE '[0-9]+\.[0-9]+' | cut -d. -f1 )
+  else
+    distro="unknown"
+  fi
+}
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+user_exist() {
+  if id "$1" >/dev/null 2>&1; then
+    echo "user $1 exists"
+  else
+    groupadd $1 ; useradd -m -d $2 -g $1 $1 
+  fi
+}
+
+file_exist() {
+  if [ -f $1 ]; then
+    cp ${1} ${1}-backup_`date +%Y%m%d%H%M%S`
+  fi
 }
 
 debian() {
@@ -70,10 +121,14 @@ unknown() {
 }
 
 installar_nrpe() {
-  git clone https://github.com/NagiosEnterprises/nrpe.git ${INSTALL_PATH}/nrpe-${NRPE_version}
-  cd ${INSTALL_PATH}/nrpe-${NRPE_version} && ./configure --prefix=/opt/nagios/ --enable-ssl --enable-command-args --with-nrpe-user=${NAGIOS_USER} --with-nrpe-group=${NAGIOS_USER} --with-nagios-user=${NAGIOS_USER} --with-nagios-group=${NAGIOS_USER} --with-opsys=linux --with-dist-type=${distro} --with-init-type=${INIT_TYPE}
-  mkdir -p /opt/nagios && groupadd -r ${NAGIOS_USER} && useradd -g ${NAGIOS_USER} -d /opt/nagios ${NAGIOS_USER} && chown -R ${NAGIOS_USER}: /opt/nagios/
+  user_exist ${NAGIOS_USER} ${INSTALL_PATH}
+  file_exist ${INSTALL_PATH}/etc/nrpe.cfg
+  git clone https://github.com/NagiosEnterprises/nrpe.git ${TEMP_PATH}/nrpe-${NRPE_version}
+  cd ${TEMP_PATH}/nrpe-${NRPE_version} && ./configure --prefix=${INSTALL_PATH} --enable-ssl --enable-command-args --with-nrpe-user=${NAGIOS_USER} --with-nrpe-group=${NAGIOS_USER} --with-nagios-user=${NAGIOS_USER} --with-nagios-group=${NAGIOS_USER} --with-opsys=linux --with-dist-type=${distro} --with-init-type=${INIT_TYPE}
+  mkdir -p /opt/nagios && groupadd -r ${NAGIOS_USER} && useradd -g ${NAGIOS_USER} -d /opt/nagios ${NAGIOS_USER} && chown -R ${NAGIOS_USER}: ${INSTALL_PATH}
   make all && make install && make install-plugin && make install-daemon && make install-config && make install-init
+  mkdir -p ${INSTALL_PATH}/etc/nrpe/
+  echo "include_dir=${INSTALL_PATH}/etc/nrpe" >>${INSTALL_PATH}/etc/nrpe.cfg
 }
 
 run_core() {
